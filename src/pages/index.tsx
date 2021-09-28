ff49ad12f14a26799a0e52a36a5ac00c7dcac043
igor goncalo <igorlabakig@gmail.com>
2021-09-28 13:48:28 +0100

    test5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="text-5xl flex justify-center items-center">
+    <div className="text-5xl flex justify-center items-center h-screen">
       <h1 className="font-semibold">Igor Labaki <span className="text-yellow-600">Goncalo</span></h1>
     </div>
   )
