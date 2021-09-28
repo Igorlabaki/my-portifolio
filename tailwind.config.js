a8fa59a2c06ca93105c1c3b6df79b98ae49d1817
igor goncalo <igorlabakig@gmail.com>
2021-09-29 00:26:05 +0100

    frontend Mentor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "column": "url('/imgs/3-column-preview-card-component.jpg')",
+        "summary": "url('/imgs/order-summary.jpg')"
+      }
+    },
   },
   variants: {
     extend: {},
