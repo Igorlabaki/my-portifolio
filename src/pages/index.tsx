0d3237a0be9e18d633ad4a0cebc954a9ff981c20
igor goncalo <igorlabakig@gmail.com>
2021-09-28 14:07:19 +0100

    s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="text-5xl flex justify-center items-center h-screen">
-      <h1 className="font-semibold">Igor Labaki <span className="text-yellow-600">Goncalo</span></h1>
+    <div className="text-5xl flex flex-col justify-center items-center h-screen bg-black">
+      <h1 className="text-white font-semibold">Igor Labaki <span className="text-yellow-600">Goncalo</span></h1>
     </div>
   )
 }
