d3e2c5e29e0acca0898bdf60ab6d3a961166884d
igor goncalo <igorlabakig@gmail.com>
2021-09-28 13:38:51 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="text-5xl flex justify-center items-center">
+      <h1 className="font-semibold">Igor Labaki <span className="text-yellow-600">Goncalo</span></h1>
+    </div>
+  )
+}
