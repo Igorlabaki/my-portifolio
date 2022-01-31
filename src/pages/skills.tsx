f7f53e9ec8290b4d3daa477681bf6bff7627d6b9
Igor Goncalo <igorlabakig@gmail.com>
2022-01-31 11:44:13 +0000

    h

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,10 +1,12 @@
 import Layout from "../components/layout/layout";
+import { FaJs, FaReact, <FaCss></FaCss> } from 'react-icons/fa';
 
 export default function about() {
     return (
         <div className="flex flex-col h-screen bg-darkBlue p-10">
             <Layout>
-           
+                <FaJs fontSize={50} color="white"/>
+                <FaReact fontSize={50} color="white"/>
             </Layout>
         </div>
     )
